25c7164fe18b0eb0659036428916fd9bd09068f4
Stacey Vanyo <sbvanyo@gmail.com>
2024-03-13 20:15:59 -0500

    signin screen

--- components/Signin.js
+++ components/Signin.js
@@ -4,22 +4,12 @@ import { signIn } from '../utils/auth';
 
 function Signin() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        margin: '0 auto',
-        zIndex: 1,
-        minHeight: '25rem',
-        width: '100%',
-        minWidth: '30rem',
-        paddingBlock: '0 5rem',
-      }}
-    >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
-      <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
+    <div id="login-screen">
+      <div id="login-msg">
+        <p>get ready to...</p>
+        <h1>ART PARTY!</h1>
+      </div>
+      <Button type="button" size="lg" id="signin-btn" onClick={signIn}>
         Sign In
       </Button>
     </div>
